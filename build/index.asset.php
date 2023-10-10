c649e8d27b271e04277c67bcf2a03b394952fbe5
Will Sides <32076464+willsides@users.noreply.github.com>
2023-10-09 23:10:22 -0700

    improved defaults

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => '0ec877eee8676b5a088d');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components'), 'version' => 'd99440133553f5f7fc8c');
