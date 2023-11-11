26189a67929d60fe696e5550ce23e3cf1a26d85d
Will Sides <32076464+willsides@users.noreply.github.com>
2023-11-10 23:09:19 -0800

    Uodate version info

--- hidden-sidebar-menu.php
+++ hidden-sidebar-menu.php
@@ -5,7 +5,7 @@
  * Description:       A menu that slides in from the right side of the screen when the user clicks the toggle button
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:           0.1.0
+ * Version:           1.0.0
  * Author:            Will Sides
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
